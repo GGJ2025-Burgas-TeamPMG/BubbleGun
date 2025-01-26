d673f4934ff17328cd3173bd4bc40b58f3ec888a
PeterSvP <petersvp@gmail.com>
2025-01-26 02:10:07 +0200

    stuff.

--- Assets/BubbleElevator.cs
+++ Assets/BubbleElevator.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -24,11 +25,28 @@ public class BubbleElevator : MonoBehaviour
         }
     }
 
+    Color originalColor;
     public void OnBalloonPooped()
     {
         var ft = GameManager.player.GetComponent<FollowTarget>();
         Destroy(ft);
+        transform.DOLocalMove(Vector3.up * 1, 1, false).SetRelative();
+        var sr = GetComponent<SpriteRenderer>();
+        originalColor = sr.color;
+        sr.DOFade(0, 1).SetEase(Ease.OutCirc);
+
         GameManager.player.rb.velocity = releaseVelocity;
         GameManager.player.ResetControls(resetAerialControl: true);
+
+        StartCoroutine(Restart());
+    }
+
+    public IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(1);
+        transform.position = GetComponent<DoTweenPathFollow>().initialPos;
+        var sr = GetComponent<SpriteRenderer>();
+        sr.DOFade(1, 1).SetEase(Ease.OutCirc);
+        GetComponent<Collider2D>().enabled = true;
     }
 }
